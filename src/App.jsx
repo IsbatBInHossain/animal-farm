f05634f292b879377728e769a66d4e10aea82f0c
Isbat-Bin-Hossain <isbat31416@gmail.com>
2023-04-03 20:49:34 +0600

    Add frontend

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,41 @@
+import { useEffect, useState } from "react";
+import Animal from "./Animal";
+
+function useAnimals() {
+  const [animals, setAnimals] = useState([]);
+
+  useEffect(() => {
+    const lastQuery = localStorage.getItem("lastQuery");
+    search(lastQuery);
+  }, []);
+  const search = async (query) => {
+    const response = await fetch(`http://localhost:5000?q=${query}`);
+    const data = await response.json();
+    localStorage.setItem("lastQuery", query);
+    setAnimals(data);
+  };
+  return [animals, search];
+}
+
 function App() {
-  return <di></di>;
+  const [animals, search] = useAnimals();
+
+  return (
+    <main>
+      <h1>Animal farm</h1>
+      <input
+        type="text"
+        placeholder="Search animal"
+        onChange={(e) => search(e.target.value)}
+      />
+      <ul>
+        {animals.map((animal) => (
+          <Animal key={animal.id} {...animal} />
+        ))}
+        {animals.length === 0 && "No animal found"}
+      </ul>
+    </main>
+  );
 }
 
 export default App;
